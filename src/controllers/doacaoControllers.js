7328d3496682efc0be03c9cb890116d59223904d
feliperafa <feliperafael_barros@hotmail.com>
2020-09-04 22:36:18 -0300

    Ajuste das rota + Ajuste doaçãoControllers

--- src/controllers/doacaoControllers.js
+++ src/controllers/doacaoControllers.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongose')
+const mongoose = require('mongoose')
 const Doacao = mongoose.model('Doacoes')
 
 exports.newDoacao = (req, res) => {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,32 +6,32 @@ module.exports = function (app) {
     //================================DOAÇÕES================================
 
     app.route('/doacoes')
-        .get(doacoes.listAll)
-        .post(doacoes.newDoacao)
+    // .get(doacoes.listAll)
 
     app.route('/doacoes/:id')
-        .get(doacoes.showOne)
-        .put(doacoes.updateUser)
-        .delete(doacoes.removerUser)
+        .post(doacoes.newDoacao)
+        // .get(doacoes.showOne)
+        // .put(doacoes.updateUser)
+        // .delete(doacoes.removerUser)
 
     //================================ONGS================================
     app.route('/ongs')
-        .get(ongs.listAll)
-        .post(ongs.creatOne)
+        // .get(ongs.listAll)
+        // .post(ongs.creatOne)
 
     app.route('/ongs/:id')
-        .get(ongs.showOne)
-        .put(ongs.updateUser)
-        .delete(ongs.removerUser)
+        // .get(ongs.showOne)
+        // .put(ongs.updateUser)
+        // .delete(ongs.removerUser)
 
     //================================VOLUNTARIOS================================
     app.route('/voluntarios')
-        .get(voluntarios.listAll)
-        .post(voluntarios.creatOne)
+        // .get(voluntarios.listAll)
+        // .post(voluntarios.creatOne)
 
     app.route('/voluntarios/:id')
-        .get(voluntarios.showOne)
-        .put(voluntarios.updateUser)
-        .delete(voluntarios.removerUser)
+        // .get(voluntarios.showOne)
+        // .put(voluntarios.updateUser)
+        // .delete(voluntarios.removerUser)
 
 }
