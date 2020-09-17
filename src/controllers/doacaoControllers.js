710b4b58bdf29464356ddb18761ea6a74e363680
Wagner Nascimento <wagneer_nascimento@live.com>
2020-09-17 17:21:47 -0300

    ajustes nas funcoes de post e get

--- src/controllers/doacaoControllers.js
+++ src/controllers/doacaoControllers.js
@@ -3,20 +3,19 @@ const Doacao = mongoose.model('Doacoes')
 const Ong = mongoose.model('Ongs')
 
 exports.newDoacao = async (req, res) => {
-
-    const { id } = req.params
-    const { nome_doador, email_doador, cpf, valor_doacao } = req.body;
+ 
+    const { nome_doador, email_doador, cpf, valor_doacao} = req.body;
     const valor_porcentagem = valor_doacao * 0.1;
     const valor_real = valor_doacao - valor_porcentagem
     let doa = new Doacao(
-        { nome_doador, email_doador, cpf, valor_doacao: valor_real, valor_doacao_sistema: valor_porcentagem, id_ong: id })
+        { nome_doador, email_doador, cpf, valor_doacao: valor_real, valor_doacao_sistema: valor_porcentagem, id_ong: req.params.id})
 
-    if (!valor_doacao || !cpf || !nome_doador || !email_doador || !id) {
+    if (!valor_doacao || !cpf || !nome_doador || !email_doador) {
         res.status(400).json('Erro favor preencha os dados em branco')
     }
 
     await doa.save({});
-    res.status(200).json('Doação Realizda com Sucesso!.')
+    res.status(200).json({doa})
 }
 
 
@@ -30,7 +29,8 @@ exports.deleteAll = (req, res) => {
 }
 
 exports.listar = async (req, res) => {
-    Doacao.findById({ _id: req.params.id }, (err, doacoes) => {
+ 
+    Doacao.find({ id_ong: req.params.id }, (err, doacoes) => {
         if (err) {
             res.send(err)
         }
